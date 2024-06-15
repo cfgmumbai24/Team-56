dfa1deb034ac36acbfe050cc0fd47de735cdec83
Somesh <sd10072004@gmail.com>
2024-06-15 23:52:09 +0530

    init

--- hunar/backend/db.js
+++ hunar/backend/db.js
@@ -1,20 +1,20 @@
 // db.js
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'urjahunar'
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "urjahunar",
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database.');
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Connected to the database.");
 });
 
 module.exports = connection;
