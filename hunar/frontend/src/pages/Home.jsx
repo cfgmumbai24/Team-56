31a5ff857fa4ceb3fa3b20a1f017aee4c59aa180
aksyt <aksyout@gmail.com>
2024-06-15 23:43:03 +0530

    init commit

--- hunar/frontend/src/pages/Home.jsx
+++ hunar/frontend/src/pages/Home.jsx
@@ -5,9 +5,7 @@ import './Home.css';
 export default function Home() {
   return (
     <div className="home-container">
-      <header className="header">
-        <h1>Gram Hunar</h1>
-      </header>
+      
       <div className="button-container">
         <Link to="/login">
           <button className="home-button">Login as a Teacher</button>
