d67e709089ef5446b1eaf2ca3ec7d5b93ce67d48
Somesh <sd10072004@gmail.com>
2024-06-15 19:15:54 +0530

    init

--- hunar/frontend/src/App.js
+++ hunar/frontend/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import EditStudentData from "./pages/EditStudentData";
+import Student from "./pages/Student";
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/student/:id" element={<Student />} />
+        <Route path="/student/:id/edit" element={<EditStudentData />} />
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
---
+++ hunar/frontend/src/pages/EditStudentData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditStudentData = () => {
+  return (
+    <div>EditStudentData</div>
+  )
+}
+
+export default EditStudentData
